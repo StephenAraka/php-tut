6e41d3c1f0a5863e33f615b12804942fa25d4e3a
StephenAraka <stevenaraka@gmail.com>
2019-10-13 21:24:02 +0300

    methods GET & POST

--- add.php
+++ add.php
@@ -3,7 +3,20 @@
 // We are checking if this submit name is set... if it's set,
 // ...then it means we have sent data to the server, and now we wanna
 // ...process that data
-if(isset($_GET['submit']))
+
+// GET METHOD - {insecure}
+// if (isset($_GET['submit'])) {
+//     echo $_GET['email'];
+//     echo $_GET['title'];
+//     echo $_GET['ingredients'];
+// }
+
+// POST METHOD - {secure}
+if (isset($_POST['submit'])) {
+    echo htmlspecialchars($_POST['email']);
+    echo htmlspecialchars($_POST['title']);
+    echo htmlspecialchars($_POST['ingredients']);
+}
 
 ?>
 
@@ -14,13 +27,13 @@ if(isset($_GET['submit']))
 
 <section class="container grey-text">
     <h4 class="center">Add a Pizza</h4>
-    <form class="white" action="add.php" method="GET">
+    <form class="white" action="add.php" method="POST">
         <label>Your Email:</label>
-        <input type="email" name="email">
+        <input type="email" name="email" required>
         <label>Pizza Title:</label>
-        <input type="text" name="title">
+        <input type="text" name="title" required>
         <label>Ingredients (comma separated):</label>
-        <input type="text" name="ingredients">
+        <input type="text" name="ingredients" required>
         <div class="center">
             <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
         </div>
