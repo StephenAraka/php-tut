e6e38ee9c2f5ab46afae177948e41f0ed354ab69
StephenAraka <stevenaraka@gmail.com>
2019-10-21 11:37:54 +0300

    render fetched data

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 $conn = mysqli_connect('localhost', 'steve', 'test1234', 'ninja_pizza');
 
 // check connection
-if(!$conn){
-  echo 'aya bass! connection error: '. mysqli_connect_error();
+if (!$conn) {
+  echo 'aya bass! connection error: ' . mysqli_connect_error();
 }
 
 // write query to get all pizzas
-$sql = 'SELECT title, ingredients, id from pizzas';
+$sql = 'SELECT title, ingredients, id from pizzas ORDER BY created_at';
 
 // make query and get result
 $result = mysqli_query($conn, $sql);
@@ -21,13 +21,35 @@ $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC); // MYSQLI_ASSOC returns assoc
 mysqli_free_result($result);
 mysqli_close($conn);
 
-print_r($pizzas);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <?php include('templates/header.php'); ?>
+
+<h4 class="center grey-text">Pizzas!</h4>
+
+<div class="container">
+  <div class="row">
+
+    <?php foreach ($pizzas as $pizza) { ?>
+
+      <div class="col s6 m3">
+        <div class="card z-depth-0">
+          <div class="card-content center">
+            <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+            <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+          </div>
+          <div class="card-action right-align">
+            <a href="#" class="brand-text">more info</a>
+          </div>
+        </div>
+      </div>
+      
+    <?php } ?>
+  </div>
+</div>
 
 <?php include('templates/footer.php'); ?>
 
