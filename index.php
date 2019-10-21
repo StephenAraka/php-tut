a8270db3a3405a4ae737c84275630a20b53ef0c5
StephenAraka <stevenaraka@gmail.com>
2019-10-21 09:29:51 +0300

    connect to db

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+// connect to db: use MySQLi or PDO
+$conn = mysqli_connect('localhost', 'steve', 'test1234', 'ninja_pizza');
+
+// check connection
+if(!$conn){
+  echo 'aya bass! connection error: '. mysqli_connect_error();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
