eb516c3de8d8cc9e8cdb7d91bfc98ffe507693fe
StephenAraka <stevenaraka@gmail.com>
2019-10-21 09:42:30 +0300

    Get data from database

--- index.php
+++ index.php
@@ -6,6 +6,22 @@ $conn = mysqli_connect('localhost', 'steve', 'test1234', 'ninja_pizza');
 if(!$conn){
   echo 'aya bass! connection error: '. mysqli_connect_error();
 }
+
+// write query to get all pizzas
+$sql = 'SELECT title, ingredients, id from pizzas';
+
+// make query and get result
+$result = mysqli_query($conn, $sql);
+
+// fetch the resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC); // MYSQLI_ASSOC returns associative array
+
+// TIP: after getting stuff from db, it's good practice to free the result from memory,
+// and close the connection
+mysqli_free_result($result);
+mysqli_close($conn);
+
+print_r($pizzas);
 ?>
 
 <!DOCTYPE html>
