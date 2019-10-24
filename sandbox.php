a283dbca13ed92163dac670ec4de1742d33df61f
StephenAraka <stevenaraka@gmail.com>
2019-10-24 13:17:47 +0300

    ternary operator

---
+++ sandbox.php
@@ -0,0 +1,19 @@
+<?php
+
+// The ternary operator
+$score = 50;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>PHP TUT</title>
+</head>
+<body>
+  <p><?php echo $score > 40 ? 'high score!' : 'low score :(' ?></p>
+</body>
+</html>
